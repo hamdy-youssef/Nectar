1a0f4109c33750ab9dfbe92417c3895e2df6cd1e
Hamdy Youssef <hamdyyoussef962@gmail.com>
2023-10-26 20:40:36 +0300

    init Application with design of ( welcome screen , login, signup)

---
+++ Nectar/View/WelcomVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomVC.swift
+//  Nectar
+//
+//  Created by Hamdy Youssef on 26/10/2023.
+//
+
+import UIKit
+
+class WelcomVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
